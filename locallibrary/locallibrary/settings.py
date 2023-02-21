bccc3fe4e480e14715b00735525dc4600e1bf7d2
Gaby1345 <60926120+Gaby1345@users.noreply.github.com>
2023-02-21 14:07:51 +0100

    settings.py deployment changes

--- locallibrary/locallibrary/settings.py
+++ locallibrary/locallibrary/settings.py
@@ -22,10 +22,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-+@=8i)r+_bht@e!z7hmh*-7kb3pwwho@3sez3f6iw94p5fvqpx'
+# SECRET_KEY = 'django-insecure-+@=8i)r+_bht@e!z7hmh*-7kb3pwwho@3sez3f6iw94p5fvqpx'
+SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'django-insecure-+@=8i)r+_bht@e!z7hmh*-7kb3pwwho@3sez3f6iw94p5fvqpx')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+# DEBUG = True
+DEBUG = os.environ.get('DJANGO_DEBUG', '') != 'False'
 
 ALLOWED_HOSTS = []
 
