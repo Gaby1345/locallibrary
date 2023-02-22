d5f08b68611cffdf55adca529406c5c31db212da
Gaby1345 <60926120+Gaby1345@users.noreply.github.com>
2023-02-22 07:44:22 +0100

    Final fix

--- build.sh
+++ build.sh
@@ -8,5 +8,6 @@ pip install -r requirements.txt
 python3 manage.py collectstatic --no-input
 python3 manage.py makemigrations
 python3 manage.py migrate
+#python3 populate_catalog.py
 
 python3 manage.py createsu
